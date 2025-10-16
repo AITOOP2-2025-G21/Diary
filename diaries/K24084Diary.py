d3c9413823f4c631933c8b361e7f37528530ae71
kaname <osoranokumokana6@gmail.com>
2025-10-16 15:35:03 +0900

    日記01_2

--- diaries/K24084Diary.py
+++ diaries/K24084Diary.py
@@ -1,8 +1,8 @@
 from diaries.AbstractDiary import AbstractDiary
-class DiarySample(AbstractDiary):
+class K24084Diary(AbstractDiary):
     def get_date(self):
-        return "2021-12-01"
+        return "2025-10-16"
     def get_summary(self):
-        return "良い一日だった"
+        return "良い一日になって欲しい"
     def get_author(self):
-        return "Sample"
+        return "kaname"
